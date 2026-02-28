0621d8a105d2b51be3f1ecb8f55cdeadde230a9d
Eduardo Zaniboni <eduardowz.prog@gmail.com>
2026-02-28 09:48:31 -0300

    finish `/api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,27 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
+
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  // console.log(JSON.stringify(responseBody, null, 2));
+
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdateAt);
+
+  const responseDatabase = responseBody.dependencies.database;
+
+  const databaseVersion = responseDatabase.version;
+  expect(responseDatabase.version).toBeDefined();
+  expect(responseDatabase.version).toEqual(databaseVersion);
+
+  const dbMaxConnections = parseInt(responseDatabase.max_connections);
+  expect(responseDatabase.max_connections).toBeDefined();
+  expect(responseDatabase.max_connections).toEqual(dbMaxConnections);
+
+  const dbConnectionsUsed = parseInt(responseDatabase.opened_connections);
+  expect(responseDatabase.opened_connections).toBeDefined();
+  expect(responseDatabase.opened_connections).toEqual(dbConnectionsUsed);
 });
