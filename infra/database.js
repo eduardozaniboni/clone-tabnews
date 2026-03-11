19c5a1233f1772ff27d73d15d69a1dcaa06d4f26
Eduardo Zaniboni <eduardowz.prog@gmail.com>
2026-03-11 06:48:15 -0300

    add `getSSLValues()` function to SSL certificate

--- infra/database.js
+++ infra/database.js
@@ -3,11 +3,11 @@ import { Client } from "pg";
 async function query(queryObject) {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT ? process.env.POSTGRES_PORT : 5432,
+    port: process.env.POSTGRES_PORT ? process.env.POSTGRES_PORT : 5433,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl: getSSLValues(),
   });
 
   try {
@@ -25,3 +25,13 @@ async function query(queryObject) {
 export default {
   query: query,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "development" ? false : true;
+}
