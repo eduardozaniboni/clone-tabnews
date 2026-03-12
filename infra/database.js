915798638bdd47b77b3da2b7404b41a59f8ea42e
Eduardo Zaniboni <eduardowz.prog@gmail.com>
2026-03-12 07:02:17 -0300

    change port to neondb

--- infra/database.js
+++ infra/database.js
@@ -17,7 +17,7 @@ async function query(queryObject) {
 async function getNewClient() {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT ? process.env.POSTGRES_PORT : 5433,
+    port: process.env.POSTGRES_PORT ? process.env.POSTGRES_PORT : 5432,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
